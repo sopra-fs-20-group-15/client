2c07a4b51ad352a54afd8c493a0ad5a954d5f99f
MinhFu <45556687+MinhFu@users.noreply.github.com>
2020-05-03 17:37:28 +0200

    FS20G15-135: Tutorial Nearly Finished

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -109,9 +109,12 @@ class Tutorial extends React.Component {
         this.state = {
             phaseNumber: 1,
             phases: ["1. Choose Number", "2. Write Clues", "3. Guess Word", "4. Word Reveal"],
+            mysteryWordId: null,
+            mysteryWords: ["Apple","Netflix","Chill","Naruto","Ghibli"],
             aliceInput: null,
             bobInput: null,
-            comment: null
+            comment: null,
+            aliceNumber: 0,
         };
     }
 
@@ -135,17 +138,52 @@ class Tutorial extends React.Component {
         }
     }
 
+    updateMysteryWord(input) {
+        for (let i = 0; i < 5; i++) {
+            if (input - 1 !== i) {
+                let lineThroughWord = document.getElementById("word" + (i + 1));
+                lineThroughWord.style.textDecoration = "line-through";
+            }
+        }
+    }
+
+    updateRightGuess() {
+        let field = document.getElementById("alice");
+        field.style.backgroundColor = "#0900ff";
+    }
+
+    updateWrongGuess() {
+        let field = document.getElementById("alice");
+        field.style.backgroundColor = "#ED0101";
+    }
+
     handleInputAlice() {
         //actions of active player
         if (this.state.phaseNumber === 1) {
             //determine mystery Word
-            this.updatePhaseHUD(2);
-            this.setState({phaseNumber:2, comment:"Well Done!"});
+            if(1<= this.state.aliceInput && this.state.aliceInput <=5){
+                this.updatePhaseHUD(2);
+                this.setState({
+                    phaseNumber:2,
+                    comment:"Well Done!",
+                    mysteryWordId: this.state.aliceInput
+                });
+                this.updateMysteryWord(this.state.aliceInput);
+            } else {
+                this.setState({comment:"Number is not between 1 and 5!"});
+            }
         }
         else if (this.state.phaseNumber === 3) {
             //guess mystery word
-            this.updatePhaseHUD(4);
-            this.setState({phaseNumber:4, comment:"You got it now!"});
+            if (this.state.aliceInput === this.state.mysteryWords[(this.state.mysteryWordId-1)]){
+                this.updatePhaseHUD(4);
+                this.setState({phaseNumber:4, comment:"You got it now!", aliceNumber: 1});
+                this.updateRightGuess();
+            } else {
+                this.updatePhaseHUD(4);
+                this.setState({phaseNumber:4, comment:"Wrong Guess but still very noice!"});
+                this.updateWrongGuess();
+            }
         }
         else if (this.state.phaseNumber === 4) {
             this.setState({comment:"The Tutorial is already over!"});
@@ -159,8 +197,12 @@ class Tutorial extends React.Component {
         //actions of passive players
         if (this.state.phaseNumber === 2) {
             //gives clue
-            this.updatePhaseHUD(3);
-            this.setState({phaseNumber:3, comment:"Very Nice!"});
+            if (this.state.bobInput !== this.state.mysteryWords[(this.state.mysteryWordId-1)]){
+                this.updatePhaseHUD(3);
+                this.setState({phaseNumber:3, comment:"Very Nice!"});
+            } else {
+                this.setState({comment:"Try to give an valid Clue please!"});
+            }
         }
         else if (this.state.phaseNumber === 4) {
             this.setState({comment:"The Tutorial is already over!"});
@@ -209,18 +251,22 @@ class Tutorial extends React.Component {
                         <InstructionText>- Phase 2: The Passive Players (indicated by the yellow Name Box) has to submit a Clue in this Phase. To do that, write in the Input</InstructionText>
                         <InstructionText style={{marginLeft:"14px"}}>Box a fitting Clue for the Mystery Word and submit it by pressing the round Button next to it. (Normally, you only control one Player
                                                                      (an active or a passive one) and you can only write and submit Inputs in your own Input Box.</InstructionText>
-                        <InstructionText></InstructionText>
+                        <InstructionText/>
+                        <InstructionText>- Phase 3: The Active Player has to submit a Guess based on the Clues the Passive Players gave (usually there are more than one</InstructionText>
+                        <InstructionText style={{marginLeft:"14px"}}>Clues available). Try submitting one!</InstructionText>
+                        <InstructionText>- Phase 4: The Guess will be checked and the Scores will be distributed. No Players has to do something in this Phase and the next</InstructionText>
+                        <InstructionText style={{marginLeft:"14px"}}>Round will begin automatically.</InstructionText>
                         <PlayerContainer style={{position:"absolute", top:"65%", right:"80%"}}>
                                 <Player style={{marginTop:"1%", marginLeft:"38%"}}>
                                     <GuessedCardsField style={{top:"10%", left:"8%"}}/>
-                                    <GuessedCardsField style={{top:"5%", left:"12%"}}>0</GuessedCardsField>
+                                    <GuessedCardsField style={{top:"5%", left:"12%"}}>{this.state.aliceNumber}</GuessedCardsField>
                                     <ScoreField>100</ScoreField>
                                         <NameFieldActivePlayer>1. Alice</NameFieldActivePlayer>
                                     <InputField>
                                         <Input placeholder="Enter here.." onChange=
                                             {e => {this.handleInputChange('aliceInput', e.target.value);}}/>)
                                     </InputField>
-                                    <ReadyField
+                                    <ReadyField id={"alice"}
                                                 onClick={() => {this.handleInputAlice();}}>
                                     </ReadyField>
                                 </Player>
@@ -249,15 +295,15 @@ class Tutorial extends React.Component {
                         </Phase>
                         <ActiveCard id={"activeCard"} style={{position:"absolute", left:"35%", bottom:"15%"}}>
                             <Number style={{color:"#00CDCD", top:"17.5px"}}> 1. </Number>
-                            <Word id={"word1"} style={{borderColor:"#00CDCD", top:"17.5px"}}> Apple </Word>
+                            <Word id={"word1"} style={{borderColor:"#00CDCD", top:"17.5px"}}> {this.state.mysteryWords[0]} </Word>
                             <Number style={{color:"#42c202", top:"65px"}}> 2. </Number>
-                            <Word id={"word2"} style={{borderColor:"#42c202", top:"35px"}}> Netflix </Word>
+                            <Word id={"word2"} style={{borderColor:"#42c202", top:"35px"}}> {this.state.mysteryWords[1]} </Word>
                             <Number style={{color:"#db3d3d", top:"112.5px"}}> 3. </Number>
-                            <Word id={"word3"} style={{borderColor:"#db3d3d", top:"52.5px"}}> Chill </Word>
+                            <Word id={"word3"} style={{borderColor:"#db3d3d", top:"52.5px"}}> {this.state.mysteryWords[2]} </Word>
                             <Number style={{color:"#fc9229", top:"160px"}}> 4. </Number>
-                            <Word id={"word4"} style={{borderColor:"#fc9229", top:"70px"}}> Naruto </Word>
+                            <Word id={"word4"} style={{borderColor:"#fc9229", top:"70px"}}> {this.state.mysteryWords[3]} </Word>
                             <Number style={{color:"#ffe203", top:"207.5px"}}> 5. </Number>
-                            <Word id={"word5"} style={{borderColor:"#ffe203", top:"87.5px"}}> Ghibli </Word>
+                            <Word id={"word5"} style={{borderColor:"#ffe203", top:"87.5px"}}> {this.state.mysteryWords[4]} </Word>
                         </ActiveCard>
                         <CommentField>{this.state.comment}</CommentField>
                     </Form>
