3e9124028869f76c7125eee18116a28f762001ef
MinhFu <45556687+MinhFu@users.noreply.github.com>
2020-05-14 07:18:02 +0200

    FS20G15-137: Implemented Game Guards
    still some errors

--- src/components/shared/routeProtectors/LobbyOverviewGuard.js
+++ src/components/shared/routeProtectors/LobbyOverviewGuard.js
@@ -4,6 +4,9 @@ import {Redirect} from "react-router-dom";
 
 export const LobbyOverviewGuard = props => {
     if (localStorage.getItem("token")) {
+        if (localStorage.getItem("GameGuard")){
+            return <Redirect to={'/gameLobby/'+localStorage.getItem("GameGuard")}/>
+        }
         return props.children;
     }
     return <Redirect to={"/login"}/>;
--- src/components/shared/routeProtectors/RegisterGuard.js
+++ src/components/shared/routeProtectors/RegisterGuard.js
@@ -5,5 +5,6 @@ import React from "react";
  * Another way to export directly your functional component.
  */
 export const RegisterGuard = props => {
+
   return props.children;
 };
