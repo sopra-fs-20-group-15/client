151bf42f61b01884b64d8d572616a00d140786b5
MinhFu <45556687+MinhFu@users.noreply.github.com>
2020-05-14 07:42:45 +0200

    FS20G15-136: Login Guard Improvement
    now logged in Users cant get to Login and Register Page

---
+++ src/components/shared/routeProtectors/GuardRegister.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Redirect} from "react-router-dom";
+
+/**
+ *
+ * Another way to export directly your functional component.
+ */
+export const GuardRegister = props => {
+    if (localStorage.getItem('token')){
+        if (localStorage.getItem("GameGuard")){
+            return <Redirect to={'/gameLobby/'+localStorage.getItem("GameGuard")}/>
+        }
+        return  <Redirect to={'/lobbyOverview'}/>
+    }
+    return props.children
+};
--- src/components/shared/routeProtectors/LoginGuard.js
+++ src/components/shared/routeProtectors/LoginGuard.js
@@ -6,8 +6,11 @@ import {Redirect} from "react-router-dom";
  * Another way to export directly your functional component.
  */
 export const LoginGuard = props => {
-  if (localStorage.getItem("GameGuard")){
-    return <Redirect to={'/gameLobby/'+localStorage.getItem("GameGuard")}/>
+  if (localStorage.getItem('token')){
+    if (localStorage.getItem("GameGuard")){
+      return <Redirect to={'/gameLobby/'+localStorage.getItem("GameGuard")}/>
+    }
+    return  <Redirect to={'/lobbyOverview'}/>
   }
   return props.children
 };
