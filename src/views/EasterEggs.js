e907d04366007bfd680d2f4536fa87f6d41dfa01
MinhFu <45556687+MinhFu@users.noreply.github.com>
2020-05-18 09:13:39 +0200

    FS20G15-174: Improved Guards
    or did I?

--- src/views/EasterEggs.js
+++ src/views/EasterEggs.js
@@ -6,6 +6,8 @@ import easterEgg3 from "./pictures/easter eggs/chicken_dance.gif";
 import easterEgg4 from "./pictures/easter eggs/trump.gif";
 import easterEgg5 from "./pictures/easter eggs/simpsons.gif";
 import easterEgg6 from "./pictures/easter eggs/pepe.gif";
+import easterEgg7 from "./pictures/easter eggs/kawaii.gif";
+import easterEgg8 from "./pictures/easter eggs/weeb.png";
 
 
 const EasterEggs = () => {
@@ -17,6 +19,8 @@ const EasterEggs = () => {
         <EasterEgg id={"mexicans"} src={easterEgg4} alt={"easter egg"} style={{width: "50%", left: "30%"}}/>
         <EasterEgg id={"milestone 4 abgabe"} src={easterEgg5} alt={"easter egg"}/>
         <EasterEgg id={"hate crime"} src={easterEgg6} alt={"easter egg"} style={{width: "50%", left: "30%"}}/>
+        <EasterEgg id={"cute"} src={easterEgg7} alt={"easter egg"} style={{width: "50%", left:"25%"}}/>
+        <EasterEgg id={"weeb"} src={easterEgg8} alt={"easter egg"} style={{width:"35%", bottom:"0%", right:"0%"}}/>
     </Fragment>
   );
 };
