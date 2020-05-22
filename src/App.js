80255bf982e62bb123e0e844b221b27268d4e5fa
cheder456 <charlotte.eder@uzh.ch>
2020-05-22 14:01:39 +0200

    FS20G15-200 Link Preview
    Test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {Helmet} from "react-helmet";
 import AppRouter from "./components/shared/routers/AppRouter";
 
 /**
@@ -9,6 +10,13 @@ class App extends Component {
   render() {
     return (
       <div>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>Just One </title>
+            <link rel="canonical" href="https://sopra-fs20-group-15-client.herokuapp.com" />
+              <meta name="A fun multiplayer game for 3 to 7 people!"/>
+              <meta property="og:image" content="./pictures/JustOneLogo.png" />
+          </Helmet>
         <AppRouter />
       </div>
     );
